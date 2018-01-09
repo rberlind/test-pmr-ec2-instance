74c799e89301687247a82bcaf3bb2e9cc4781a3d
Roger Berlind <rberlind@optonline.net>
2018-01-09 18:02:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,8 @@ module "ec2" {
   version = "1.1.0"
 
   ami = "ami-aa2ea6d0"
-  associate_public_ip_address = "true"
   instance_type = "t2.micro"
   name = "pmr-test-by-roger"
   subnet_id = "subnet-c656fae9"
-  vpc_security_group_ids = "sg-cb387fbe"
+  vpc_security_group_ids = ["sg-cb387fbe"]
 }
